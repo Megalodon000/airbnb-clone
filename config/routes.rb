caac8555ee587b9cdb4f91509841fdc2cf6aa670
ali mercade <alimercad@gmail.com>
2024-05-21 23:08:39 +0100

    wishlist controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   #get "up" => "rails/health#show", as: :rails_health_check
+
+
+  namespace :api do
+    resources :wishlists, only: [:create, :destroy]
+  end
 end
